e5d68f7194e5ad009d3312d249742e32ab18cd6e
Serena <serenadagher.90@gmail.com>
2024-11-13 02:59:06 +0200

    api created

---
+++ models/ski.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const SkiSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  brand: {
+    type: String,
+    required: true
+  },
+  type: { // Specify the type of ski (e.g., All-mountain, Freeride, Freestyle)
+    type: String,
+    required: true
+  },
+  length: { // Length in centimeters
+    type: Number,
+    required: true
+  },
+  widthWaist: { // Width of the ski at its narrowest point
+    type: Number,
+    required: true
+  },
+  description: {
+    type: String
+  },
+  available: { // Flag to indicate if the ski is available for rent
+    type: Boolean,
+    default: true
+  },
+  image: { // URL or path to an image of the ski
+    type: String
+  },
+  created_date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Ski = mongoose.model('Ski', SkiSchema);
