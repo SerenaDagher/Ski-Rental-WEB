5849a31b0b6b2a3a677adbd693b776a143a79c4b
Serena <serenadagher.90@gmail.com>
2024-11-15 12:52:13 +0200

    home page added

---
+++ ski-rental/src/components/HomePage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Carousel, Navbar, Nav, Container } from "react-bootstrap";
+// import "../App.css";
+
+function HomePage() {
+  return <h1>Hello, world!</h1>;
+}
+
+export default HomePage;
--- ski-rental/src/main.jsx
+++ ski-rental/src/main.jsx
@@ -1,25 +1,45 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import "./index.css";
+
+// // Bootstrap CSS & JS imports
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/js/bootstrap.bundle.min";
+
+// // Components imports
+// import HomePage from "./components/HomePage";
+// import CreateRental from "./components/CreateRental";
+// // import RentalCard from "./components/RentalCard";
+// import ShowRentalDetails from "./components/ShowRentalDetails";
+// // import ShowRentalList from "./components/ShowRentalList";
+// import UpdateRentalInfo from "./components/UpdateRentalInfo";
+
+// // Routes
+// const router = createBrowserRouter([
+//   { path: "/", element: <HomePage /> },
+//   // { path: "/create-rental", element: <CreateRental /> },
+//   // { path: "/rental/:id", element: <ShowRentalDetails /> },
+//   // { path: "/edit-rental/:id", element: <UpdateRentalInfo /> },
+// ]);
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <RouterProvider router={router} />
+//   </React.StrictMode>
+// );
+
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-
-// Bootstrap CSS & JS imports
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
+import HomePage from "./components/HomePage";
 
-// Components imports
-import CreateRental from "./components/CreateRental";
-import RentalCard from "./components/RentalCard";
-import ShowRentalDetails from "./components/ShowRentalDetails";
-import ShowRentalList from "./components/ShowRentalList";
-import UpdateRentalInfo from "./components/UpdateRentalInfo";
-
-// Routes
+// Define routes with just HomePage
 const router = createBrowserRouter([
-  { path: "/", element: <ShowRentalList /> },
-  { path: "/create-rental", element: <CreateRental /> },
-  { path: "/rental/:id", element: <ShowRentalDetails /> },
-  { path: "/edit-rental/:id", element: <UpdateRentalInfo /> },
+  { path: "/", element: <HomePage /> },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -27,3 +47,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <RouterProvider router={router} />
   </React.StrictMode>
 );
+
+
