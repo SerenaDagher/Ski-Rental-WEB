10fb04b3319cccc8ee2a7f5df0cf5bc2fd3f6f4a
yaracham <yaraelcham@hotmail.com>
2024-11-18 21:23:57 +0200

    added ui pagination

--- ski-rental/src/App.jsx
+++ ski-rental/src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
-import React, { useState } from "react";
+import React, { useState ,useRef } from "react";
 import Signup from "./components/SignUp";
 import TopBar from "./components/TopBar"; 
 import MyCarousel from "./components/MyCarousel";
+import Pagination  from './components/Pagination';
+import { Paper } from '@mui/material';
+
 
 function App() {
+  console.log("App is rendering!");
+
   const [showSignup, setShowSignup] = useState(false);
+
 
   return (
     <div className="App" style = {{zIndex: 1}}>
@@ -19,8 +25,11 @@ function App() {
         </>
       )}
       <MyCarousel />
-
+      <Paper elevation={6}>
+        <Pagination/>
+      </Paper>
     </div>
+
   )
 }
 
