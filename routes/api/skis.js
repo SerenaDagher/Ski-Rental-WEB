4bbe96f76ff48ab91db1cf19ca47a253bb9cb9d6
yaracham <yaraelcham@hotmail.com>
2024-11-19 12:07:56 +0200

    fetching skis from database working api call

--- routes/api/skis.js
+++ routes/api/skis.js
@@ -2,55 +2,43 @@
 const express = require('express');
 const router = express.Router();
 
-// Load Book model
-const Book = require('../../models/ski');
 
-// @route   GET api/books/test
-// @desc    Tests books route
-// @access  Public
+const Ski = require('../../models/ski');
+
+
 router.get('/test', (req, res) => res.send('ski route testing!'));
 
 router.get('/', (req, res) => {
-  Book.find()
-    .then(books => res.json(books))
+  Ski.find()
+    .then(skis => res.json(skis))
     .catch(err => res.status(404).json({ nobooksfound: 'No skis found' }));
 });
 
-// @route   GET api/books/:id
-// @desc    Get single book by id
-// @access  Public
+
 router.get('/:id', (req, res) => {
-  Book.findById(req.params.id)
-    .then(book => res.json(book))
+  Ski.findById(req.params.id)
+    .then(ski => res.json(book))
     .catch(err => res.status(404).json({ nobookfound: 'No skis found' }));
 });
 
-// @route   POST api/books
-// @desc    Add/save book
-// @access  Public
+
 router.post('/', (req, res) => {
-  Book.create(req.body)
-    .then(book => res.json({ msg: 'ski added successfully' }))
+  Ski.create(req.body)
+    .then(ski => res.json({ msg: 'ski added successfully' }))
     .catch(err => res.status(400).json({ error: 'Unable to add this ski' }));
 });
 
-// @route   PUT api/books/:id
-// @desc    Update book by id
-// @access  Public
 router.put('/:id', (req, res) => {
-  Book.findByIdAndUpdate(req.params.id, req.body)
-    .then(book => res.json({ msg: 'Updated successfully' }))
+  Ski.findByIdAndUpdate(req.params.id, req.body)
+    .then(ski => res.json({ msg: 'Updated successfully' }))
     .catch(err =>
       res.status(400).json({ error: 'Unable to update the Database' })
     );
 });
 
-// @route   DELETE api/books/:id
-// @desc    Delete book by id
-// @access  Public
 router.delete('/:id', (req, res) => {
-  Book.findByIdAndDelete(req.params.id)
-    .then(book => res.json({ mgs: 'ski entry deleted successfully' }))
+  Ski.findByIdAndDelete(req.params.id)
+    .then(ski => res.json({ mgs: 'ski entry deleted successfully' }))
     .catch(err => res.status(404).json({ error: 'No such ski' }));
 });
 
